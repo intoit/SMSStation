082937fce3dbc3e1e202545b18104444e9de5079
Lauri Savolainen <lauri.savolainen@intoit.fi>
2014-09-02 13:52:12 +0300

    Initial push, just testing Maven

---
+++ SMSStation/src/main/java/fi/intoit/smsstation/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fi.intoit.smsstation;
+
+/**
+ *
+ * @author lasa
+ */
+public class Main {
+    public static void main(String[] Args) {
+        System.out.println("TESTINGS");
+        
+    }
+}
