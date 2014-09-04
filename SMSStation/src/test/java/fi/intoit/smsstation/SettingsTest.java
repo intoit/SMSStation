6089f6d30938bf167f21380a8476e0c783568544
Lauri Savolainen <lauri.savolainen@intoit.fi>
2014-09-04 22:33:03 +0300

    Just testing Tests

---
+++ SMSStation/src/test/java/fi/intoit/smsstation/SettingsTest.java
@@ -0,0 +1,26 @@
+package fi.intoit.smsstation;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author lasa
+ */
+
+public class SettingsTest {
+
+    @Test
+    public void tryAlwaysPasstest() {
+
+    }
+    @Test
+    public void storeGSMNumber() {
+    assert(false);
+    }
+    @Test
+    public void getGSMNumber() {
+    assert(true);
+    }
+}
