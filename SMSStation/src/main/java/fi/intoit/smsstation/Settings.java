85587fd6b7d592da9c33b817d82b3c6911f99ca7
Lauri Savolainen <lauri.savolainen@intoit.fi>
2014-09-10 19:26:20 +0300

    Modified settings

--- SMSStation/src/main/java/fi/intoit/smsstation/Settings.java
+++ SMSStation/src/main/java/fi/intoit/smsstation/Settings.java
@@ -11,11 +11,30 @@ package fi.intoit.smsstation;
  */
 public class Settings {
     private String GSMNumber;
-    
+    private String Username;
+    private String Password;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+            
     public String getGSMNumber() {
         return GSMNumber;
     }
     public void setGSMNumber(String number) {
         this.GSMNumber = number;
     }
+    
 }
