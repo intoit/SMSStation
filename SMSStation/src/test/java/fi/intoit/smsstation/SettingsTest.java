39cf548869c3468d85c6a4a071cee91fbf4f8969
Lauri Savolainen <lauri.savolainen@intoit.fi>
2014-09-10 19:33:44 +0300

    Refactored Settings test And updated project definitions

--- SMSStation/src/test/java/fi/intoit/smsstation/SettingsTest.java
+++ SMSStation/src/test/java/fi/intoit/smsstation/SettingsTest.java
@@ -10,10 +10,14 @@ import static org.junit.Assert.*;
  */
 
 public class SettingsTest {
-
+    Settings settings;
+    @Before
+    public void setupUp() {
+        settings = new Settings();
+    }
     @Test
     public void getRightGSMNumber() {
-        Settings settings = new Settings();
+
         String number = "0404040404";
         settings.setGSMNumber(number);
         assertEquals("Set number is same as get",number,settings.getGSMNumber());
